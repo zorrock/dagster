e369a018393c857c08613a34f05222b2e2290160
Nicholas Schrock <schrockn@elementl.com>
2019-04-14 07:53:01 -0700

    cp

--- python_modules/airline-demo/setup.py
+++ python_modules/airline-demo/setup.py
@@ -30,6 +30,7 @@ setup(
     install_requires=[
         'boto3==1.9.*',
         'dagster',
+        'dagster_aws',
         'dagstermill',
         'descartes==1.1.0',
         'geopandas==0.4.0',
