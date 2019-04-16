2c73e0ef343bb649b522f2a146ed0f0043cf8c69
Max Gasner <max.gasner@gmail.com>
2019-04-16 15:02:31 -0700

    Add notebook solid

--- examples/event-pipeline-demo/event_pipeline_demo/pipelines.py
+++ examples/event-pipeline-demo/event_pipeline_demo/pipelines.py
@@ -10,6 +10,8 @@ from dagster import (
     InputDefinition,
     OutputDefinition,
     List,
+    Nothing,
+    Path,
     PipelineDefinition,
     SolidInstance,
     String,
@@ -19,6 +21,7 @@ from dagster.utils import safe_isfile, mkdir_p
 from dagster_aws import download_from_s3
 from dagster_spark import SparkSolidDefinition
 from dagster_snowflake import SnowflakeSolidDefinition, SnowflakeLoadSolidDefinition
+from dagstermill import define_dagstermill_solid
 
 
 @lambda_solid(inputs=[InputDefinition('gzip_file', String)], output=OutputDefinition(List(String)))
@@ -40,6 +43,18 @@ def gunzipper(gzip_file):
     return [path_prefix]
 
 
+geo_events = define_dagstermill_solid(
+    'geo_events',
+    os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'notebooks',
+        'Geographic_Distribution_of_Recent_Events.ipynb',
+    ),
+    inputs=[InputDefinition('table_ready', Nothing)],
+    outputs=[OutputDefinition(Path, 'path')],
+)
+
+
 def define_event_ingest_pipeline():
     event_ingest = SparkSolidDefinition(
         name='event_ingest',
@@ -57,12 +72,13 @@ def define_event_ingest_pipeline():
 
     return PipelineDefinition(
         name='event_ingest_pipeline',
-        solids=[download_from_s3, gunzipper, event_ingest, snowflake_load],
+        solids=[download_from_s3, gunzipper, event_ingest, snowflake_load, geo_events],
         dependencies={
             SolidInstance('gunzipper'): {'gzip_file': DependencyDefinition('download_from_s3')},
             SolidInstance('event_ingest'): {'spark_inputs': DependencyDefinition('gunzipper')},
             SolidInstance('snowflake_load'): {
                 SnowflakeSolidDefinition.INPUT_READY: DependencyDefinition('event_ingest', 'paths')
             },
+            'geo_events': {'table_ready': DependencyDefinition('snowflake_load')},
         },
     )
